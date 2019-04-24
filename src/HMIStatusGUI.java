208c37fa631dc8a6b43f0d351438a66065437f4f
Alanx94 <s1129910@windesheim.nl>
2019-04-24 13:30:15 +0200

    WERKENDE PIJLEN

--- src/HMIStatusGUI.java
+++ src/HMIStatusGUI.java
@@ -7,6 +7,8 @@ public class HMIStatusGUI extends JFrame implements ActionListener {
 
     private int roodAantal = 0, groenAantal = 0, blauwAantal = 0, pijlX = 0;
 
+    private String kleur; // Hier moet de kleur inkomen die de sensor waarneemt
+
     private JButton jbHome, jbStart, jbStop, jbResultaat;
     private JLabel jlRood, jlGroen, jlBlauw;
 
@@ -22,7 +24,7 @@ public class HMIStatusGUI extends JFrame implements ActionListener {
         setTitle("Status robots");
 
         // timer met delay van 1 seconde
-        timer = new Timer(500, this);
+        timer = new Timer(150, this);
 
         tekenPanel = new TekenPanel(this);
 
@@ -58,29 +60,56 @@ public class HMIStatusGUI extends JFrame implements ActionListener {
 
 
         // teken en beweeg de pijlen op de lopende band
-    public void beweegPijlen(Graphics g){
+    public void beweegPijlen(Graphics g, String kleur){
+        // stelt de kleur van de pijlen in
+        if (kleur == null) {
+            g.setColor(Color.GREEN);
+        } else if (kleur == "blauw") {
+            g.setColor(Color.cyan);
+        } else if (kleur == "rood") {
+            g.setColor(Color.RED);
+        } else if (kleur == "geel") {
+            g.setColor(Color.YELLOW);
+        }
 
-        g.setColor(Color.GREEN);
+        int y = (HoofdschermGUI.getSchermHoogte() - 200) / 2;
+        // aanmaken arrays voor de X en Y punten van driehoek (pijlpunt)
+        int[] xpoints = new int[3];
+        int[] ypoints = new int[3];
 
-        int y = HoofdschermGUI.getSchermHoogte() / 2;
+        for (int i = 1; i < 7; i++) {
+            // aantal blokjes, hun positie en bewegen met stappen van pijlX
+            int x = (90 * i) + pijlX;
 
-        for (int i = 1; i < 8; i++) {
-            int x = (50 * i) + pijlX;
+            // X punten van driehoek toevoegen aan array
+            xpoints[0] = x + 31;
+            xpoints[1] = x + 55;
+            xpoints[2] = x + 31;
+            // Y punten van driehoek toevoegen aan array
+            ypoints[0] = y - 15;
+            ypoints[1] = y + 10;
+            ypoints[2] = y + 35;
 
+            //print de x en y positie van de blokjes en de i van de loop
             System.out.println(x * i + " " + y + " " + i);
             System.out.println(timer);
-            g.fillRect(x, y, 20, 20);
+
+            // teken pijlbody
+            g.fillRect(x, y, 30, 20);
+            // teken pijlpunt
+            g.fillPolygon(xpoints, ypoints, 3);
 
         }
+        // verplaats de pijlen met een verandering van 20 naar rechts
         pijlX += 20;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
-            // Met interval van de delay van de timer wordt dit gedaan:
+        // Met interval van de delay van de timer wordt dit gedaan:
         if (e.getSource() == timer) {
-            if (pijlX == 120) {
+            if (pijlX == 120) { // zet de x terug naar begin en reset de posities van de pijlen elke keer
                 pijlX = 0;
             }
         }
@@ -89,6 +118,7 @@ public class HMIStatusGUI extends JFrame implements ActionListener {
         if (e.getSource() == jbStart) {
             timer.start();
         }
+
         // wanneer op de stopknop wordt gedrukt gebeurd dit:
         if (e.getSource() == jbStop) {
             timer.stop();
